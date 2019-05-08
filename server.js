49a5073d2b14200e848def2b11aa00459da0ebab
Ryan Carville <ryancarville@gmail.com>
2019-05-08 19:17:08 +0200

    fixed port error

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use((error, req, res, next) => {
 	res.status(500).json(response);
 });
 
-const PORT = process.env.NODE_ENV || 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
 	console.log(`Listening to Port ${PORT}`);
